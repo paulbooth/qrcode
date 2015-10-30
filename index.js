cc9c7bba6ff0e70574221e598013351842da4a3c
Paul Booth <thephantompaulbooth@gmail.com>
2015-10-30 02:03:40 -0700

    Add favicon.

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var express = require('express'),
 var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.urlencoded());
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 var server = http.createServer(app);
